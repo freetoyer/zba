5d7785a3e3a148380090dc1e05c293775f7e7efb
freetoyer <andrey_89-89@mail.ru>
2020-08-02 22:42:12 +0300

    cleaned code

--- text_analyzer/urls.py
+++ text_analyzer/urls.py
@@ -5,5 +5,4 @@ from .views import TextAnalyzerView, textview
 
 urlpatterns = [
     path('', TextAnalyzerView.as_view(), name='text_analyzer'),
-    path('<str:text>/', textview, name='text'),
 ]
--- text_analyzer/views.py
+++ text_analyzer/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from django.http import JsonResponse
-from django.urls import reverse
 from django.views import View
 
 import re
@@ -15,19 +13,12 @@ class TextAnalyzerView(View):
         return render(request, 'text_analyzer.html', {'form': form})
     
     def post(self, request):
-        # form = TextForm(request.POST)
-        # if form.is_valid():
-        #     text = form.cleaned_data
-        #     return HttpResponseRedirect(reverse('text', args=(text,)))
         text = request.POST.get('text')
         text = re.sub('.*&text=', '', text)
         return JsonResponse({
             'text': text
             })
-        
-        
-def textview(request, text):
-    return render(request, 'text.html')
+
 
     
 
